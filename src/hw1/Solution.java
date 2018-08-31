0f5ad7b8f17bc5cd230ed0e28cd031c168cef6dc
HLF_SUN <sunhlf@HLF-SUNs-MacBook-Pro.local>
2018-08-31 13:40:04 -0400

    resummit

---
+++ src/hw1/Solution.java
@@ -0,0 +1,13 @@
+package hw1;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Solution {
+    List<T> result = new ArrayList<>();
+
+    List<T> longestPrefix(List<T> a, List<T> b, Comparator<? super T> cmp){
+        return result;
+    }
+}
